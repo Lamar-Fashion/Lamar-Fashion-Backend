5b4b73b0bb5e85d8ea0b57c37025a1d651dafe34
Ahmad-AbuOsbeh <ahmadabuosbeh20@gmail.com>
2022-07-20 19:15:57 +0300

    added favourite routes & middlewares

--- src/routers/api.route.js
+++ src/routers/api.route.js
@@ -17,23 +17,83 @@ const permissions = require('../middlewares/acl');
 
 // api routes
 apiRouter.post('/favourite', bearerAuth, addToFavouriteHandler);
-// apiRouter.get('/favourite', bearerAuth,  getFavouriteHandler);
-// apiRouter.delete('/favourite', bearerAuth, removeFavouriteHandler);
+apiRouter.get('/favourite/:userId', bearerAuth,  getFavouriteHandler);
+apiRouter.delete('/favourite/:userId/:id', bearerAuth, removeFavouriteHandler);
 apiRouter.post('/addToCart', addToCartHandler);
 apiRouter.get('/allProducts', allProductsHandler);
 apiRouter.get('/homePageProducts', homePageProductsHandler);
 
 // add To Favourite Handler
 async function addToFavouriteHandler(req, res, next) {
-  try {
-    // save favourite & user ids
-    const response = await favouritCollection.create(req.body);
-    console.log('response', response);
+  console.log('req.body', req.body);
+  const {userId , abayaId} = req.body;
 
+
+  try {
+    let favFromDB = await favouritCollection.model.findOne({where:{userId:userId.toString() }});
+    let favObj ,response;
+if (favFromDB) {
+
+  // prevent duplicates items
+  !favFromDB.abayaId.includes(abayaId.toString()) ? favFromDB.abayaId = [...favFromDB.abayaId,abayaId.toString()] : null;
+
+  //update the obj
+  response = await favFromDB.save();
+}else{
+  favObj = {
+    userId,
+    abayaId: [abayaId.toString()]
+  }
+
+  // save first favourite record for this user
+  response = await favouritCollection.create(favObj);
+}
+     
     // return the object  to the client
-    res.status(201).send(response);
+    res.status(200).send(response);
   } catch (e) {
     next(e.message, 'add to favourite error');
+  }
+}
+// get favourites handler
+async function getFavouriteHandler(req, res, next) {
+  const {userId} = req.params;
+  try {
+    const userFavRecord = await favouritCollection.read(userId);
+
+const favPromises = userFavRecord[0]?.abayaId?.map(async(favId)=>{
+  const favItem = await abayaCollection.model.findOne({
+    where:{id : favId } });
+    return favItem;
+})
+    const allFavItems  = await Promise.all(favPromises);
+
+    res.status(200).send(allFavItems);
+  } catch (error) {
+    next(error.message, 'get favourites error');
+  }
+}
+// remove favourites handler
+async function removeFavouriteHandler(req, res, next) {
+  const {userId,id} = req.params;
+  try {
+    let userFavRecord = await favouritCollection.model.findOne({where:{userId:userId.toString() }});
+
+    userFavRecord?.abayaId?.map((favId,idx)=>{
+if (favId == id) {
+  userFavRecord.abayaId.splice(idx,1);
+  return;
+}
+});
+console.log('userFavRecord',userFavRecord);
+const newAbayaIds = userFavRecord.abayaId;
+userFavRecord.abayaId= [];
+userFavRecord.abayaId= newAbayaIds;
+ await userFavRecord.save({ fields: ['abayaId'] });
+ let response = await userFavRecord.reload();
+ response ?  res.status(200).send(true) : res.status(200).send(null) ;
+  } catch (error) {
+    next(error.message, 'remove favourite error');
   }
 }
 
