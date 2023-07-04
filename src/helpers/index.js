f3808acba50dded0d9b8ce6000fa3e388fee868e
Ahmad-AbuOsbeh <ahmadabuosbeh20@gmail.com>
2023-07-04 05:09:42 +0300

    updated whatsapp template for order summary

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -32,38 +32,15 @@ function checkExpirationDate (stringDate, dateNow) {
 function generateOrderSummaryMessage (order) {
     const personalInfo = order.personalInfo;
     const productInfo = order.productInfo;
-    // let summary = `Hi ${personalInfo.Fname ?? '' } ${personalInfo.Lname ?? ''},\n\nThank you for your recent purchase from Lamar Fashion. We are pleased to confirm your order. Here's a summary of your order:\n\nProduct(s) Ordered:\n\n`;
-
-    // // Loop over the products and concatenate the code and quantity to the string
-    // for (let i = 0; i < productInfo.length; i++) {
-    //     summary += `- Product Code: ${productInfo[i].code}, Size: ${productInfo[i].size}, Quantity: ${productInfo[i].quantity}, Original Price: ${productInfo[i].price} QAR\n`;
-    //     summary += `Product Link: ${process.env.Website_URL}/ProductDetails/${productInfo[i].id}\n\n`;
-    // }
-    
-    // // Add the total price to the string
-    // summary += `\nTotal Price: ${order.totalPrice} QAR`;
-    
-    // // Add the contact details to the string
-    // summary += `\n\nWe'll use the phone number (${personalInfo.phone}) and email (${personalInfo.email}) you provided to get in touch.`;
-
-    // summary += `\n\nOrder will be shipped to:`;
-    // summary += `\n${personalInfo.FlatNumber} [Building No.], ${personalInfo.StreetAddress} [Street], ${personalInfo.Zone}, ${personalInfo.city}, ${personalInfo.country}.`;
-
-    // summary+= `\n\nIf you have any questions or concerns about your order, please don't hesitate to contact us. We're always here to help!\n\nThanks again for choosing our brand. We hope to see you again soon.\n\nBest regards,\n\nLamar Fashion Team.\n`;
-
-    // // Add the contact details to the string
-    // summary += `Phone: ${process.env.LAMAR_Phone_Number}\nEmail: ${process.env.NODE_MAILER_EMAIL_SENDER}`;
-
-    // return summary;
 
     let products = "";
     for (let i = 0; i < productInfo.length; i++) {
-        products += `- Product Code: ${productInfo[i].code}, Size: ${productInfo[i].size}, Quantity: ${productInfo[i].quantity}, Original Price: ${productInfo[i].price} QAR\n`;
-        products += `Product Link: ${process.env.Website_URL}/ProductDetails/${productInfo[i].id}\n\n`;
+        products += `${i+1}- Product Code: ${productInfo[i].code}, Size: ${productInfo[i].size}, Quantity: ${productInfo[i].quantity}, Original Price: ${productInfo[i].price} QAR **`;
+        products += `Product Link: ${process.env.Website_URL}/ProductDetails/${productInfo[i].id} >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>`;
     };
 
-    let summary = `Hi ${personalInfo.Fname || '' + personalInfo.Lname || ''},\n\nThank you for your recent purchase from Lamar Fashion.\nWe are pleased to confirm your order.\n\nHere's a summary of your order:\n\nProduct(s) Ordered:\n\n${products}Total Price: ${order.totalPrice} QAR.\n\nWe'll use this phone number ${personalInfo.phone} and email ${personalInfo.email} you provided to get in touch.\nOrder will be shipped to: ${personalInfo.FlatNumber + " [Building No.], " + personalInfo.StreetAddress + " [Street], " + personalInfo.Zone + ", " + personalInfo.city + ", " + personalInfo.country + "."} soon.\n\nIf you have any questions or concerns about your order, please don't hesitate to contact us. We're always here to help!\n\nThanks again for choosing our brand.\nWe hope to see you again soon.\n\nBest regards,\nLamar Fashion Team.\nPhone: ${process.env.LAMAR_Phone_Number}\nEmail: ${process.env.NODE_MAILER_EMAIL_SENDER}\n\n**NOTE: This Whatsapp number used only for notifying you when your order successfully completed. so please don't reply to this chat, but reach out to our phone number instead: ${process.env.LAMAR_Phone_Number}.`;
-    
+    let summary = `Hi ${(personalInfo.Fname || '') + (personalInfo.Lname || '')},\n\nThank you for your recent purchase from Lamar Fashion.\nWe are pleased to confirm your order.\n\nHere's a summary of your order:\n\nProduct(s) Ordered:\n\n${products}Total Price: ${order.totalPrice} QAR.\n\nWe'll use this phone number ${personalInfo.phone} and email ${personalInfo.email} you provided to get in touch.\nOrder will be shipped to: ${personalInfo.FlatNumber + " [Building No.], " + personalInfo.StreetAddress + " [Street], " + personalInfo.Zone + ", " + personalInfo.city + ", " + personalInfo.country + "."} soon.\n\nIf you have any questions or concerns about your order, please don't hesitate to contact us. We're always here to help!\n\nThanks again for choosing our brand.\nWe hope to see you again soon.\n\nBest regards,\nLamar Fashion Team.\nPhone: ${process.env.LAMAR_Phone_Number}\nEmail: ${process.env.NODE_MAILER_EMAIL_SENDER}\n\n**NOTE: This Whatsapp number used only for notifying you when your order successfully completed. so please don't reply to this chat, but reach out to our phone number instead: ${process.env.LAMAR_Phone_Number}.`
+
     //Whatsapp Template:
     // Hi {{1}},\n\nThank you for your recent purchase from Lamar Fashion.\nWe are pleased to confirm your order.\n\nHere's a summary of your order:\n\nProduct(s) Ordered:\n\n{{2}}Total Price: {{3}} QAR.\n\nWe'll use this phone number {{4}} and email {{5}} you provided to get in touch.\nOrder will be shipped to: {{6}} soon.\n\nIf you have any questions or concerns about your order, please don't hesitate to contact us. We're always here to help!\n\nThanks again for choosing our brand.\nWe hope to see you again soon.\n\nBest regards,\nLamar Fashion Team.\nPhone: {{7}}\nEmail: {{8}}\n\n**NOTE: This Whatsapp number used only for notifying you when your order successfully completed. so please don't reply to this chat, but reach out to our phone number instead: {{7}}.
 
